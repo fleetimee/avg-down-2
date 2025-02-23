f2fd6d331a82a2010b83b1694b387be4991dd653
Novian Andika <novian.andika@students.amikom.ac.id>
2025-02-23 09:59:37 +0700

    Remove database connection test from db.ts

--- src/app/db.ts
+++ src/app/db.ts
@@ -4,13 +4,4 @@ const db = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-// Test the connection
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection error:", err.stack);
-  } else {
-    console.log("Successfully connected to database");
-  }
-});
-
 export default db;
